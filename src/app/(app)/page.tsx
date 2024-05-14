bac2db36613af60cca6b9722f7d11e9b8257211f
Abhishek Saini <abhisheksaini655@gmail.com>
2024-05-14 09:55:32 +0530

    some changes

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -13,6 +13,7 @@ import {
   CarouselItem
 } from '@/components/ui/carousel';
 import { useSession } from 'next-auth/react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
@@ -20,9 +21,46 @@ export default function Home() {
   
   return (
     <>
-      
+      <section>
+      <div className="relative w-full bg-gray-800">
+        <div className="mx-auto max-w-7xl lg:px-8">
+          <div className="flex flex-col justify-center px-4 py-10 lg:px-6">
+            <div className="mt-10 flex max-w-max items-center space-x-2 rounded-full border p-2">
+              <p className="text-xs text-gray-200 font-medium md:text-sm">
+                Lorem ipsum dolor sit amet consectetur.
+                <span className="ml-2 cursor-pointer font-bold">Read More &rarr;</span>
+              </p>
+            </div>
+            <h1 className="mt-8 max-w-4xl text-3xl font-bold tracking-tight text-white md:text-4xl lg:text-6xl">
+              Dive into the World of Anonymous Feedback
+            </h1>
+            <p className="mt-8 max-w-3xl text-lg text-gray-300">
+            True Feedback - Where your identity remains a secret.
+            </p>
+            <div className="mt-4">
+            {!session && <Link href="/sign-up">
+            <Button className='mt-3 bg-black  text-lg px-3 py-1' >Create a free account
+              </Button></Link>}
+          {session && <Link href="/dashboard">
+            <Button className='mt-3 bg-black text-lg' >Dashboard
+              </Button></Link>}
+            </div>
+          </div>
+          {/* <div className="rounded-lg bg-gray-200 p-4"> */}
+          <div>
+            <Image
+              className="aspect-[3/2] w-full rounded-lg bg-gray-50 object-cover lg:aspect-auto lg:h-[500px] lg:object-center"
+              src="/new-message.svg"
+              width={500}
+              height={500}
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      </section>
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
-        <section className="text-center mb-8 md:mb-12">
+        {/* <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
             Dive into the World of Anonymous Feedback
           </h1>
@@ -36,7 +74,9 @@ export default function Home() {
           {session && <Link href="/dashboard">
             <Button className='mt-3 bg-black text-lg' >Dashboard
               </Button></Link>}
-        </section>
+        </section> */}
+
+        
 
         
         <Carousel
